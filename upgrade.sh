02bc47091d7fb47894887ac2457b1ca9badd1890
VinZ <vinsmano@gmail.com>
2024-12-28 11:31:17 +0100

    comment

--- upgrade.sh
+++ upgrade.sh
@@ -1,5 +1,18 @@
 #!/bin/bash 
 
+VERSIONEBBQ=`cat repo/kodivins/addon.xml | grep version | grep addon | grep -oP 'version="\K[0-9]+'`
+echo "Versione corrente $VERSIONEBBQ"
+echo
+NEWVERSIONEBBQ=$((VERSIONEBBQ + 1))
+echo "Nuova versione $NEWVERSIONEBBQ"
+echo
+echo
+updated_input=$( sed -i  -E "s/version=\"$VERSIONEBBQ\"/version=\"$NEWVERSIONEBBQ\"/")
+VERSIONEBBQ=`cat repo/kodivins/addon.xml | grep version | grep addon | grep -oP 'version="\K[0-9]+'`
+echo "Versione corrente $VERSIONEBBQ"
+echo
+
+
 rm -rf repo/zips
 
 python _repo_generator.py
